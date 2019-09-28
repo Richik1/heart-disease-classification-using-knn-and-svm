6ef60b4077dfe1086234c0840b530503394a2b01
Richik1 <55250423+Richik1@users.noreply.github.com>
2019-09-28 17:49:28 +0530

    Add files via upload

---
+++ heardisease_classifier.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 28 17:35:06 2019
+
+@author: KIIT
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+# We are reading our data
+df = pd.read_csv("heart.csv")
+df.target.value_counts()
+sns.countplot(x="target", data=df, palette="bwr")
+plt.show()
+
+countNoDisease = len(df[df.target == 0])
+countHaveDisease = len(df[df.target == 1])
+print("Percentage of Patients Haven't Heart Disease: {:.2f}%".format((countNoDisease / (len(df.target))*100)))
+print("Percentage of Patients Have Heart Disease: {:.2f}%".format((countHaveDisease / (len(df.target))*100)))
+df.groupby('target').mean()
+pd.crosstab(df.age,df.target).plot(kind="bar",figsize=(20,6))
+plt.title('Heart Disease Frequency for Ages')
+plt.xlabel('Age')
+plt.ylabel('Frequency')
+plt.savefig('heartDiseaseAndAges.png')
+plt.show()
+plt.scatter(x=df.age[df.target==1], y=df.thalach[(df.target==1)], c="red")
+plt.scatter(x=df.age[df.target==0], y=df.thalach[(df.target==0)])
+plt.legend(["Disease", "Not Disease"])
+plt.xlabel("Age")
+plt.ylabel("Maximum Heart Rate")
+plt.show()
+a = pd.get_dummies(df['cp'], prefix = "cp")
+b = pd.get_dummies(df['thal'], prefix = "thal")
+c = pd.get_dummies(df['slope'], prefix = "slope")
+frames = [df, a, b, c]
+df = pd.concat(frames, axis = 1)
+df.head()
+df = df.drop(columns = ['cp', 'thal', 'slope'])
+df.head()
+# KNN Model
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors = 2)  # n_neighbors means k
+knn.fit(x_train.T, y_train.T)
+prediction = knn.predict(x_test.T)
+
+print("{} NN Score: {:.2f}%".format(2, knn.score(x_test.T, y_test.T)*100))
+# try ro find best k value
+scoreList = []
+for i in range(1,20):
+    knn2 = KNeighborsClassifier(n_neighbors = i)  # n_neighbors means k
+    knn2.fit(x_train.T, y_train.T)
+    scoreList.append(knn2.score(x_test.T, y_test.T))
+    
+plt.plot(range(1,20), scoreList)
+plt.xticks(np.arange(1,20,1))
+plt.xlabel("K value")
+plt.ylabel("Score")
+plt.show()
+
+acc = max(scoreList)*100
+accuracies['KNN'] = acc
+print("Maximum KNN Score is {:.2f}%".format(acc))
+from sklearn.svm import SVC
+svm = SVC(random_state = 1)
+svm.fit(x_train.T, y_train.T)
+
+acc = svm.score(x_test.T,y_test.T)*100
+accuracies['SVM'] = acc
+print("Test Accuracy of SVM Algorithm: {:.2f}%".format(acc))
